96799832b122fe4d558b1b95c645c2f00b6c1ff5
Andrew Shockley <andrew_shockley@gallagherre.com>
2024-07-31 10:12:49 -0500

    Initial commit
    Signed-off-by: Andrew Shockley <andrew_shockley@gallagherre.com>

---
+++ R/test_fn.R
@@ -0,0 +1,19 @@
+#' Test function
+#'
+#' @author Andrew Shockley
+#' @description
+#' This function adds one to a number.
+#' 
+#' @usage test_fn(x)
+#' 
+#' @param x A number
+#' 
+#' @return x incremented by 1
+#' 
+#' @examples
+#' num_plus_one <- test_fn(1)
+#' 
+#' @export
+test_fn <- function(x) {
+  return(x + 1)
+}
